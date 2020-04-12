f96ca301c3fa24630c9401986db93fa63f3a8906
Sudarshan Giri <41857483+SudarshanGiri@users.noreply.github.com>
2020-04-12 13:37:41 +0545

    removed unnecessary insert

--- submit_vote.php
+++ submit_vote.php
@@ -3,24 +3,40 @@
 	session_start();
 	session_destroy();
 	$key = 'qkwjdiw239&&jdafweihbrhnan&^%$ggdnawhd4njshjwuuO';
-	function encryptthis($data, $key) { 
-	$encryption_key = base64_decode($key); $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc')); $encrypted = openssl_encrypt($data, 'aes-256-cbc', $encryption_key, 0, $iv);
-	return base64_encode($encrypted . '::' . $iv); }
+	function encryptthis($data, $key) { //encryption function
+	$encryption_key = base64_decode($key); 
+	$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc')); 
+	$encrypted = openssl_encrypt($data, 'aes-256-cbc', $encryption_key, 0, $iv);
+	return base64_encode($encrypted . '::' . $iv); }///end encryption function
 	$pres_id=$_SESSION[pres_id];
 	$pres_id=encryptthis($pres_id,$key);
+	$vpinternal_id=$_SESSION[vpinternal_id];
+	$vpinternal_id=encryptthis($vpinternal_id,$key);
+	$vpexternal_id=$_SESSION[vpexternal_id];
+	$vpexternal_id=encryptthis($vpexternal_id,$key);
+	$secretary_id=$_SESSION[secretary_id];
+	$secretary_id=encryptthis($secretary_id,$key);
+	$auditor_id=$_SESSION[auditor_id];
+	$auditor_id=encryptthis($auditor_id,$key);
+	$treasurer_id=$_SESSION[treasurer_id];
+	$treasurer_id=encryptthis($treasurer_id,$key);
+	
+	
+
+
 
 
 		$conn->query("INSERT INTO `votes` VALUES('', '$pres_id', '$_SESSION[voters_id]')") or die(mysql_error());
-		$conn->query("INSERT INTO `votes` VALUES('', '$_SESSION[vpinternal_id]', '$_SESSION[voters_id]')") or die(mysql_error());
-		$conn->query("INSERT INTO `votes` VALUES('', '$_SESSION[vpexternal_id]', '$_SESSION[voters_id]')") or die(mysql_error());
-		$conn->query("INSERT INTO `votes` VALUES('', '$_SESSION[secretary_id]', '$_SESSION[voters_id]')") or die(mysql_error());
-		$conn->query("INSERT INTO `votes` VALUES('', '$_SESSION[auditor_id]', '$_SESSION[voters_id]')") or die(mysql_error());
-		$conn->query("INSERT INTO `votes` VALUES('', '$_SESSION[treasurer_id]', '$_SESSION[voters_id]')") or die(mysql_error());
-		$conn->query("INSERT INTO `votes` VALUES('', '$_SESSION[pio_id]', '$_SESSION[voters_id]')") or die(mysql_error());
-		$conn->query("INSERT INTO `votes` VALUES('', '$_SESSION[busman_id]', '$_SESSION[voters_id]')") or die(mysql_error());
-		$conn->query("INSERT INTO `votes` VALUES('', '$_SESSION[sgtarm_id]', '$_SESSION[voters_id]')") or die(mysql_error());
-		$conn->query("INSERT INTO `votes` VALUES('', '$_SESSION[muse_id]', '$_SESSION[voters_id]')") or die(mysql_error());
-		$conn->query("INSERT INTO `votes` VALUES('', '$_SESSION[escort_id]', '$_SESSION[voters_id]')") or die(mysql_error());
+		$conn->query("INSERT INTO `votes` VALUES('', '$vpinternal_id', '$_SESSION[voters_id]')") or die(mysql_error());
+		$conn->query("INSERT INTO `votes` VALUES('', '$vpexternal_id', '$_SESSION[voters_id]')") or die(mysql_error());
+		$conn->query("INSERT INTO `votes` VALUES('', '$secretary_id', '$_SESSION[voters_id]')") or die(mysql_error());
+		$conn->query("INSERT INTO `votes` VALUES('', '$auditor_id', '$_SESSION[voters_id]')") or die(mysql_error());
+		$conn->query("INSERT INTO `votes` VALUES('', '$treasurer_id', '$_SESSION[voters_id]')") or die(mysql_error());
+	//	$conn->query("INSERT INTO `votes` VALUES('', '$pio_id', '$_SESSION[voters_id]')") or die(mysql_error());
+	//	$conn->query("INSERT INTO `votes` VALUES('', '$busman_id', '$_SESSION[voters_id]')") or die(mysql_error());
+	//	$conn->query("INSERT INTO `votes` VALUES('', '$sgtarm_id', '$_SESSION[voters_id]')") or die(mysql_error());
+	//	$conn->query("INSERT INTO `votes` VALUES('', '$muse_id', '$_SESSION[voters_id]')") or die(mysql_error());
+	//	$conn->query("INSERT INTO `votes` VALUES('', '$escort_id', '$_SESSION[voters_id]')") or die(mysql_error());
 		$conn->query("UPDATE `voters` SET `status` = 'Voted' WHERE `voters_id` = '$_SESSION[voters_id]'") or die(mysql_error());
 		header("location:index.php");
 		
